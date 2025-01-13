2fc767fce8884ba8c41d60f09002d2d815125684
premkumar-004 <premkumarkardale@gmail.com>
2025-01-13 15:52:03 +0530

    tailwind configuration testing

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,11 +1,10 @@
-/** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
-    "./src/*./jsx",
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
